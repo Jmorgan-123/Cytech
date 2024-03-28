727ae5ed8bf7f877f5b47f10a217a4c2b643edb5
Gamer Jzs <165060451+Jmorgan-123@users.noreply.github.com>
2024-03-28 22:41:39 +0300

    Update classRectangle.h

--- reference code/area_of_shapes/C++_code/classRectangle.h
+++ reference code/area_of_shapes/C++_code/classRectangle.h
@@ -1,52 +1,13 @@
 //
-// Created by Victor Wachira on 04/02/2024.
+// Created by Eng Joab Mugane on 18/03/2024.
 //
 
+ } else if (choice == 2) {
+            System.out.print("Enter the length of the rectangle: ");
+            double length = scanner.nextDouble();
+            System.out.print("Enter the width of the rectangle: ");
+            double width = scanner.nextDouble();
+            double area = calculateRectangleArea(length, width);
+            System.out.println("The area of the rectangle is: " + area);
 
-using namespace std;
-class Rectangle
-{
-private:
-    double len;
-    double wid;
-
-public:
-    double getLength() const;
-    double getWidth() const;
-    double getArea() const;
-    double getPerimeter() const;
-    void setValues();
-
-    ~Rectangle() = default;
-};
-
-//Class definition
-double Rectangle::getLength() const
-{
-    return len;
-}
-
-double Rectangle::getWidth() const {
-    return wid;
-}
-
-double Rectangle::getPerimeter() const {
-    return(2*(len+wid));
-}
-
-double Rectangle::getArea() const {
-    return(len*wid);
-}
-
-void Rectangle::setValues() {
-    double length;
-    double width;
-    cout<<"Enter the length"<<endl;
-    cin>>length;
-    cout<<"Enter the width: "<<endl;
-    cin>>width;
-    len= length;
-    wid=width;
-
-}
-
+   
