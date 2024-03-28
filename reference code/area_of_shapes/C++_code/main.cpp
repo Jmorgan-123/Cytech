ce61bbfbe291cbc4946a9986f7578b6ad0c52557
Gamer Jzs <165060451+Jmorgan-123@users.noreply.github.com>
2024-03-28 22:22:08 +0300

    Update main.cpp

--- reference code/area_of_shapes/C++_code/main.cpp
+++ reference code/area_of_shapes/C++_code/main.cpp
@@ -1,76 +1,60 @@
-#include <iostream>
-#include "classRectangle.h"
-#include "classCircle.h"
-#include "ClassTriangle.h"
-#include "classSquare.h"
-using namespace std;
+using System;
 
-int main() {
-    //
-    int choice=0;
-    int enterChoice=0;
-    cout<<"Would you like to calculate area of shapes: "<<endl;
-    cout<<"1. yes.\n2. no\n"<<endl;
-    cin>>choice;
-
-    while (choice==1) {
-        cout<<"To calculate area of shape, choose the shape number below:"<<endl;
-        cout<<"1. Circle.\n2. Rectangle\n3. triangle\n4. Square"<<endl;
-        cin>>enterChoice;
-        if (enterChoice==2) {
-
-            cout << "Rectangle1" << endl;
-            Rectangle rectangle1{};
-            rectangle1.setValues();
-            cout << "Length:\t" << rectangle1.getLength() << endl;
-            cout << "Width:\t" << rectangle1.getWidth() << endl;
-            cout << "Perimeter:\t" << rectangle1.getPerimeter() << endl;
-            cout << "Area:\t" << rectangle1.getArea()<<" Square units" << endl << endl;
-
-
-        }
-
-        else if (enterChoice==1) {
-            //creating first circle and applying member functions
-            cout << "Circle 1:" << endl;
-            classCircle circle1{};
-            circle1.setRadius();
-            classCircle circle2{};
-            cout<<"Circle 2"<<endl;
-            circle2.setRadius();
-            cout << "Radius: " << circle1.getRadius() << endl;
-            cout << "Area: " << circle1.getArea() <<" Square units."<< endl;
-            cout << "Perimeter: " << circle1.getPerimeter() << endl << endl;
-
-
-            cout << "Radius: " << circle2.getRadius() << endl;
-            cout << "Area: " << circle2.getArea()<<"Square units" << endl;
-            cout << "Perimeter: " << circle2.getPerimeter() << endl << endl;
-        }
-
-        else if(enterChoice==3){
-            ClassTriangle triangle1{};
-            triangle1.setValues();
-            cout<<"Triangle 1"<<endl;
-            cout<<"Height:\t"<<triangle1.getHeight()<<endl;
-            cout<<"Base:\t"<<triangle1.getBase()<<endl;
-            cout<<"Area:\t"<<triangle1.getArea()<<endl<<endl;
-        }
-
-        else if(enterChoice==4){
-            Square square1{};
-            square1.setValues();
-            cout<<"Side: "<<square1.getSide()<<endl;
-            cout<<"Area: "<<square1.getArea()<<endl;
-            cout<<"Perimeter: "<<square1.getPerimeter()<<endl<<endl;
-
-
-        }
-        cout<<"Would you like to continue: "<<endl;
-        cout<<"1. yes\n2. no"<<endl<<endl;
-        cin>>choice;
-
+class Program
+{
+    static double CalculateCircleArea(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
     }
 
-    return 0;
+    static double CalculateRectangleArea(double length, double width)
+    {
+        return length * width;
+    }
+
+    static double CalculateTriangleArea(double baseLength, double height)
+    {
+        return 0.5 * baseLength * height;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Choose a shape:");
+        Console.WriteLine("1. Circle");
+        Console.WriteLine("2. Rectangle");
+        Console.WriteLine("3. Triangle");
+
+        Console.Write("Enter your choice (1/2/3): ");
+        int choice = int.Parse(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            Console.Write("Enter the radius of the circle: ");
+            double radius = double.Parse(Console.ReadLine());
+            double area = CalculateCircleArea(radius);
+            Console.WriteLine($"The area of the circle is: {area}");
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Enter the length of the rectangle: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            double width = double.Parse(Console.ReadLine());
+            double area = CalculateRectangleArea(length, width);
+            Console.WriteLine($"The area of the rectangle is: {area}");
+        }
+        else if (choice == 3)
+        {
+            Console.Write("Enter the base of the triangle: ");
+            double baseLength = double.Parse(Console.ReadLine());
+            Console.Write("Enter the height of the triangle: ");
+            double height = double.Parse(Console.ReadLine());
+            double area = CalculateTriangleArea(baseLength, height);
+            Console.WriteLine($"The area of the triangle is: {area}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice.");
+        }
+    }
 }
