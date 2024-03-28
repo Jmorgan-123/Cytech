1f3c082ae077555e6045f1771c09f60af4c1d65f
Gamer Jzs <165060451+Jmorgan-123@users.noreply.github.com>
2024-03-28 22:46:33 +0300

    Update classSquare.h

--- reference code/area_of_shapes/C++_code/classSquare.h
+++ reference code/area_of_shapes/C++_code/classSquare.h
@@ -1,36 +1,10 @@
 //
-// Created by Victor Wachira on 05/02/2024.
+// Created by Eng Joab Mugane on 05/02/2024.
 //
 
-#include<cmath>
-class Square{
-private:
-    double side{};
-public:
-    double getSide() const;
-    double getPerimeter() const;
-    double getArea() const;
-    void setValues();
+} else if (choice == 4) {
+            System.out.print("Enter the side length of the square: ");
+            double sideLength = scanner.nextDouble();
+            double area = calculateSquareArea(sideLength);
+            System.out.println("The area of the square is: " + area);
 
-    ~Square()=default;
-
-};
-
-double Square::getSide() const {
-    return side;
-}
-
-double Square::getArea() const {
-    return(pow(side,2));
-}
-
-double Square::getPerimeter() const {
-    return(4*(side));
-}
-
-void Square::setValues() {
-    double value;
-    cout << "Enter the value for the side of the square: ";
-    cin >> value;
-    side = value;
-}
